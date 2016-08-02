487097e97cbe6c530f72a89a23ad29807b0969f3
Light1of1mind <light_of_mind@mail.ru>
2016-08-02 16:47:34 +0300

    erst

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace WindowsFormsApplication1
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread]feFSEFregfreg
         static void Main()
         {
             Application.EnableVisualStyles();
