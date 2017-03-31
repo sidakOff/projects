d7509f5111e8fc5106601621b4054d9d10b4828d
Light1of1mind <light_of_mind@mail.ru>
2017-03-31 17:17:53 +0300

    приложение для освоения многопоточности

---
+++ TestPicture/TestPicture/SelectForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace TestPicture
+{
+    partial class SelectPictureForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.originalPictureBox = new System.Windows.Forms.PictureBox();
+            this.changedPictureBox = new System.Windows.Forms.PictureBox();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.blueTextBox = new System.Windows.Forms.TextBox();
+            this.greenTextBox = new System.Windows.Forms.TextBox();
+            this.redTextBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.changedPictureBox)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // selectButton
+            // 
+            this.selectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.selectButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.selectButton.Location = new System.Drawing.Point(3, 478);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(366, 52);
+            this.selectButton.TabIndex = 0;
+            this.selectButton.Text = "Выбрать картинку";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 207F));
+            this.tableLayoutPanel1.Controls.Add(this.selectButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.originalPictureBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.changedPictureBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.changeButton, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.2495F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.75051F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(951, 533);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // originalPictureBox
+            // 
+            this.originalPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.originalPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.originalPictureBox.Name = "originalPictureBox";
+            this.originalPictureBox.Size = new System.Drawing.Size(366, 469);
+            this.originalPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.originalPictureBox.TabIndex = 1;
+            this.originalPictureBox.TabStop = false;
+            // 
+            // changedPictureBox
+            // 
+            this.changedPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.changedPictureBox.Location = new System.Drawing.Point(375, 3);
+            this.changedPictureBox.Name = "changedPictureBox";
+            this.changedPictureBox.Size = new System.Drawing.Size(366, 469);
+            this.changedPictureBox.TabIndex = 2;
+            this.changedPictureBox.TabStop = false;
+            // 
+            // changeButton
+            // 
+            this.changeButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.changeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.changeButton.Location = new System.Drawing.Point(375, 478);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(366, 52);
+            this.changeButton.TabIndex = 3;
+            this.changeButton.Text = "Изменить";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.75124F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.24876F));
+            this.tableLayoutPanel2.Controls.Add(this.redTextBox, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.greenTextBox, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.blueTextBox, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(747, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(201, 469);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Синий(0-255)";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Зеленый(0-255)";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Красный(0-255)";
+            // 
+            // blueTextBox
+            // 
+            this.blueTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.blueTextBox.Location = new System.Drawing.Point(102, 10);
+            this.blueTextBox.Name = "blueTextBox";
+            this.blueTextBox.Size = new System.Drawing.Size(96, 20);
+            this.blueTextBox.TabIndex = 3;
+            // 
+            // greenTextBox
+            // 
+            this.greenTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.greenTextBox.Location = new System.Drawing.Point(102, 50);
+            this.greenTextBox.Name = "greenTextBox";
+            this.greenTextBox.Size = new System.Drawing.Size(96, 20);
+            this.greenTextBox.TabIndex = 4;
+            // 
+            // redTextBox
+            // 
+            this.redTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.redTextBox.Location = new System.Drawing.Point(102, 90);
+            this.redTextBox.Name = "redTextBox";
+            this.redTextBox.Size = new System.Drawing.Size(96, 20);
+            this.redTextBox.TabIndex = 5;
+            // 
+            // SelectPictureForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 533);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "SelectPictureForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Выбери картинку";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.changedPictureBox)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox originalPictureBox;
+        private System.Windows.Forms.PictureBox changedPictureBox;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox redTextBox;
+        private System.Windows.Forms.TextBox greenTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox blueTextBox;
+    }
+}
+
---
+++ TestPicture/TestPicture/SelectForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestPicture
+{
+    public partial class SelectPictureForm : Form
+    {
+        private Image _image;
+
+        public SelectPictureForm()
+        {
+            InitializeComponent();
+        }
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            string startPath;
+            GetImage(out startPath);
+        }
+
+        private void GetImage(out string startPath)
+        {
+            startPath = @"C:\";
+            OpenFileDialog theDialog = new OpenFileDialog
+            {
+                Title = "Выберите файл"
+            };
+            theDialog.InitialDirectory = startPath;
+            theDialog.Multiselect = false;
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = theDialog.FileName;
+
+                const Int32 BufferSize = 128;
+                using (var fileStream = File.OpenRead(fileName))
+                {
+                    _image = Image.FromStream(fileStream);
+                    originalPictureBox.Image = _image;
+                }
+
+                //using (var streamReader = new StreamReader(fileStream, Encoding.Default, true, BufferSize))
+                //{
+
+                //}
+            }
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            var ima = _image;
+            Bitmap img = ima as Bitmap;
+            var pixels=new List<Pixel>();
+            int  blue = Convert.ToInt32(blueTextBox.Text);
+            int green= Convert.ToInt32(greenTextBox.Text);
+            int red = Convert.ToInt32(redTextBox.Text);
+            for (int i = 0; i < ima.Width; i++)
+            {
+                for (int j = 0; j < ima.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+
+                    if ((pixel.B <=blue)&(pixel.G<=green)&(pixel.R<=red))
+                    {
+                        pixels.Add(new Pixel()
+                        {
+                            i = i,
+                            j = j,
+                            pix = pixel
+                        });
+                    }
+                }
+            }
+            //int maxAge = FindMax(list, x => x.Age);
+            var width = pixels.Max(o => o.i)+1;
+            var height = pixels.Max(o => o.j)+1;
+            var image=new Bitmap(width, height);
+            foreach (var pix in pixels)
+            {
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        image.SetPixel(pix.i, pix.j, pix.pix);
+                    }
+                }
+            }
+            changedPictureBox.Image = image;
+
+        }
+        public class Pixel
+        {
+            public int i { get; set; }
+            public int j { get; set; }
+            public Color pix { get;set; }
+        }
+    }
+}
