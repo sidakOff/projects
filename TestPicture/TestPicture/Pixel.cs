7104ca1e0c350c27345989a90741251cafcc60c2
Light1of1mind <light_of_mind@mail.ru>
2017-04-03 11:57:15 +0300

    Что-то сделал

---
+++ TestPicture/TestPicture/Pixel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPicture
+{
+    public class Pixel
+    {
+        public int i { get; set; }
+        public int j { get; set; }
+        public Color pix { get; set; }
+    }
+}
--- TestPicture/TestPicture/SelectForm.cs
+++ TestPicture/TestPicture/SelectForm.cs
@@ -16,7 +16,10 @@ namespace TestPicture
     public partial class SelectPictureForm : Form
     {
         private Image _image;
-
+        private int width;
+        private int height;
+        private Bitmap image;
+        
         public SelectPictureForm()
         {
             InitializeComponent();
@@ -41,27 +44,51 @@ namespace TestPicture
             {
                 var fileName = theDialog.FileName;
 
-                const Int32 BufferSize = 128;
                 using (var fileStream = File.OpenRead(fileName))
                 {
                     _image = Image.FromStream(fileStream);
                     originalPictureBox.Image = _image;
                 }
-
-                //using (var streamReader = new StreamReader(fileStream, Encoding.Default, true, BufferSize))
-                //{
-
-                //}
             }
         }
 
         private void changeButton_Click(object sender, EventArgs e)
         {
+            var image = DrawImage();
+            changedPictureBox.Image = image;
+        }
+
+        private Bitmap DrawImage()
+        {
+            
+            var pixels = GetPixelsFromImage();
+            width = pixels.Max(o => o.i) + 1;
+            height = pixels.Max(o => o.j) + 1;
+            image = new Bitmap(width, height);
+
+
+            foreach (var pix in pixels)
+            {
+               Drawining(pix);
+            }
+            return image;
+        }
+
+        private void Drawining(object obj)
+        {
+            var pix = (Pixel) obj;
+            image.SetPixel(pix.i, pix.j, pix.pix);
+            
+        }
+        
+
+        private List<Pixel> GetPixelsFromImage()
+        {
             var ima = _image;
             Bitmap img = ima as Bitmap;
-            var pixels=new List<Pixel>();
-            int  blue = Convert.ToInt32(blueTextBox.Text);
-            int green= Convert.ToInt32(greenTextBox.Text);
+            var pixels = new List<Pixel>();
+            int blue = Convert.ToInt32(blueTextBox.Text);
+            int green = Convert.ToInt32(greenTextBox.Text);
             int red = Convert.ToInt32(redTextBox.Text);
             for (int i = 0; i < ima.Width; i++)
             {
@@ -69,9 +96,9 @@ namespace TestPicture
                 {
                     Color pixel = img.GetPixel(i, j);
 
-                    if ((pixel.B <=blue)&(pixel.G<=green)&(pixel.R<=red))
+                    if ((pixel.B >= blue) & (pixel.G >= green) & (pixel.R >= red))
                     {
-                        pixels.Add(new Pixel()
+                        pixels.Add(new Pixel
                         {
                             i = i,
                             j = j,
@@ -80,28 +107,7 @@ namespace TestPicture
                     }
                 }
             }
-            //int maxAge = FindMax(list, x => x.Age);
-            var width = pixels.Max(o => o.i)+1;
-            var height = pixels.Max(o => o.j)+1;
-            var image=new Bitmap(width, height);
-            foreach (var pix in pixels)
-            {
-                for (int i = 0; i < image.Width; i++)
-                {
-                    for (int j = 0; j < image.Height; j++)
-                    {
-                        image.SetPixel(pix.i, pix.j, pix.pix);
-                    }
-                }
-            }
-            changedPictureBox.Image = image;
-
-        }
-        public class Pixel
-        {
-            public int i { get; set; }
-            public int j { get; set; }
-            public Color pix { get;set; }
+            return pixels;
         }
     }
 }
